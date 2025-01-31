ef7b7cf6d9c315c034dae90fe594cc0af30324ac
cassiaqueiroz <cassiarsqueiroz@gmail.com>
2025-01-30 23:52:41 -0300

    Primeira versão com animação dos Planos

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,17 +172,3 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
-
-document.addEventListener("DOMContentLoaded", () => {
-  const plansInner = document.querySelector('#plans .plans-inner');
-  
-  // Pausa a animação ao passar o mouse
-  plansInner.addEventListener("mouseenter", () => {
-      plansInner.style.animationPlayState = 'paused';
-  });
-
-  // Retoma a animação ao remover o mouse
-  plansInner.addEventListener("mouseleave", () => {
-      plansInner.style.animationPlayState = 'running';
-  });
-});
